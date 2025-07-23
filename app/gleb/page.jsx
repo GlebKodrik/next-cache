baab833530b34744aa1cf4a8e571632879c4f4de
Gleb <kodrikgleb2@gmail.com>
2025-07-23 23:27:59 +0300

    обновил

--- app/gleb/page.jsx
+++ app/gleb/page.jsx
@@ -1,3 +1,4 @@
+import {TAGS} from "./../_lib/tags";
 
 
 export const metadata = {
@@ -5,6 +6,14 @@ export const metadata = {
 };
 
 export default async function Page() {
+  const response = await fetch('https://6881440066a7eb81224a8608.mockapi.io/test/user', {next: {tags: [TAGS.INSIGHT]}});
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+
   const now = new Date();
   const hours = String(now.getHours()).padStart(2, '0');
   const minutes = String(now.getMinutes()).padStart(2, '0');
@@ -15,6 +24,8 @@ export default async function Page() {
    <>
      <div style={{ color: 'red' }}>
        Время {timeWithSeconds}
+
+         {data?.map((item) => (<div>{item.name} + {item.id}</div>))}
      </div>
    </>
   );
